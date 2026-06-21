f08eea4b0b1c9a4d686c2f59ff823b2616e79cfe
wspnggas82 <ramanbarzan948@gmail.com>
2026-06-21 22:32:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// دیاریکردنی فۆڵدەری پۆبڵیک بۆ فایلە جێگیرەکان (CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
+// ئێستا ڕەگی سەرەکی (تەنیشت server.js) وەک شوێنی فایلە جێگیرەکان (CSS, JS) دیاری کراوە
+app.use(express.static(__dirname));
 
-// ڕاوتەری سەرەکی بۆ کردنەوەی پەیجەکە
+// ڕاوتەری سەرەکی بۆ کردنەوەی پەیجەکە ڕاستەوخۆ لە ڕەگی سەرەکییەوە
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// کارپێکردنی سێرڤەر لەسەر پۆرت پۆرت ٣٠٠٠
-const PORT = process.env.PORT || 3000; // لێرە بیکە بە 8080
+// کارپێکردنی سێرڤەر لەسەر پۆرت ٣٠٠٠ یان پۆرتی هۆستەکە
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`سێرڤەرەکە کاردەکات لەسەر: http://localhost:${PORT}`);
 });
